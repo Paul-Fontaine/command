325b7e1a8b0e2957aedd34d62eb51f52409443da
Paul Fontaine <fontainepaul36@gmail.com>
2024-02-02 15:24:22 +0100

    add reverse command

--- string_commands_window.py
+++ string_commands_window.py
@@ -2,6 +2,7 @@ from PySide6.QtWidgets import QWidget, QPushButton, QLineEdit, QVBoxLayout, QHBo
 from command_string_invoker import StringCommandInvoker
 from to_lower import ToLower
 from to_upper import ToUpper
+from reverse import Reverse
 from my_string import MyString
 
 
@@ -14,6 +15,8 @@ class StringCommandWindow(QWidget):
         self.__invoker.add_command('to lower', tolower)
         toupper = ToUpper(self.__my_string)
         self.__invoker.add_command('to upper', toupper)
+        reverse = Reverse(self.__my_string)
+        self.__invoker.add_command('reverse', reverse)
 
         self.__line_edit: QLineEdit = QLineEdit(self)
         self.__line_edit.setPlaceholderText('type here ...')
@@ -21,6 +24,8 @@ class StringCommandWindow(QWidget):
         self.__button_lower.clicked.connect(self.__invoke_lower_case_command)
         self.__button_upper: QPushButton = QPushButton('upper case', self)
         self.__button_upper.clicked.connect(self.__invoke_upper_case_command)
+        self.__button_reverse: QPushButton = QPushButton('reverse', self)
+        self.__button_reverse.clicked.connect(self.__invoke_reverse_command)
 
         vlayout = QVBoxLayout(self)
         hlayout1 = QHBoxLayout(self)
@@ -30,6 +35,7 @@ class StringCommandWindow(QWidget):
 
         hlayout2.addWidget(self.__button_lower)
         hlayout2.addWidget(self.__button_upper)
+        hlayout2.addWidget(self.__button_reverse)
 
         vlayout.addLayout(hlayout1)
         vlayout.addLayout(hlayout2)
@@ -42,4 +48,9 @@ class StringCommandWindow(QWidget):
     def __invoke_upper_case_command(self):
         self.__my_string.set_string(self.__line_edit.text())
         self.__invoker.invoke_command('to upper')
+        self.__line_edit.setText(self.__my_string.get_string())
+
+    def __invoke_reverse_command(self):
+        self.__my_string.set_string(self.__line_edit.text())
+        self.__invoker.invoke_command('reverse')
         self.__line_edit.setText(self.__my_string.get_string())
